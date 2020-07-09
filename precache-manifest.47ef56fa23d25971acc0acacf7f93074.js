10ed28d7d29aeddb00fbf5ec21a42b80b0a9afe0
Rakesh Dmello <rakesh.dmello@gmail.com>
2020-07-10 09:51:56 +1000

    Updates

---
+++ precache-manifest.47ef56fa23d25971acc0acacf7f93074.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3de7fdb41e55005ef729e48dfaebe82",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "be3488b66dee9e3201eb",
+    "url": "/robofriends/static/css/main.ecf3808d.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3488b66dee9e3201eb",
+    "url": "/robofriends/static/js/main.6823ae54.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
